0bab3fba140802bd8970d3ed0ec4be7b8ec5f1c2
f0rest96 <jovan.marijamladenovic@gmail.com>
2021-10-17 22:12:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,75 @@
+# Frontend Mentor - Rock, Paper, Scissors solution
+
+This is a solution to the [Rock, Paper, Scissors challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the game depending on their device's screen size
+- Play Rock, Paper, Scissors against the computer
+- Maintain the state of the score after refreshing the browser _(optional)_
+- **Bonus**: Play Rock, Paper, Scissors, Lizard, Spock against the computer _(optional)_
+
+### Screenshot
+
+![Game](https://github.com/f0rest96/rock-paper-scissors-main/blob/master/screenshots/game.png)
+
+![Win-Screen](https://github.com/f0rest96/rock-paper-scissors-main/blob/master/screenshots/win.png)
+
+![Lose-Screen](https://github.com/f0rest96/rock-paper-scissors-main/blob/master/screenshots/lose.png)
+
+![Draw-Screen](https://github.com/f0rest96/rock-paper-scissors-main/blob/master/screenshots/draw.png)
+
+![Rules-Screen](https://github.com/f0rest96/rock-paper-scissors-main/blob/master/screenshots/rulse.png)
+
+### Links
+
+- Solution URL: [Github repo](https://github.com/f0rest96/rock-paper-scissors-main)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS custom properties
+- JavaScript
+### What I learned
+
+With this project i learned how to user flexboxes, and add event listeners to buttons, also i learned how to style and add animations via CSS
+
+I learned  how to easely make a simple button animation using transform property and scale()
+```css
+.btn:active {
+    transform: scale(0.9);
+}
+```
+### Continued development
+
+In the future i will attempt on adding two more modules and restyle the whole site to be fully responsive.
+
+### Useful resources
+
+- [CSSgradient](https://cssgradient.io) - This helped me for making custom gradients and adding it to the site.
+## Author
+
+- Website - [Still WIP](#)
+- Frontend Mentor - [@f0rest96](https://www.frontendmentor.io/profile/f0rest96)
+- Github - [@f0rest96](https://github.com/f0rest96)
+
---
+++ script.js
@@ -0,0 +1,98 @@
+const buttons = document.querySelectorAll('.pick');
+const scoreEl = document.getElementById('score');
+const main = document.getElementById('main');
+const selection = document.getElementById('selection');
+const reset = document.getElementById('reset');
+const user_select = document.getElementById('user_select');
+const computer_select= document.getElementById('computer_select');
+const winner = document.getElementById('winner');
+
+// modal buttons
+
+const modal = document.getElementById('modal');
+const openBtn = document.getElementById('open');
+const closeBtn = document.getElementById('close');
+
+const choices = ['paper', 'rock', 'scissors'];
+
+let score = 0;
+let userChoice = undefined;
+
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        userChoice = button.getAttribute('data-choice');
+
+        checkWinner();
+    });
+});
+
+reset.addEventListener('click', () => {
+    // show the selection | hide main
+    main.style.display = 'flex';
+    selection.style.display = 'none';
+});
+
+openBtn.addEventListener('click', () => {
+    // show the selection | hide main
+    modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    // show the selection | hide main
+    modal.style.display = 'none';
+});
+
+function checkWinner() {
+    const computerChoice = pickRandomChoice();
+
+    // update the view
+    updateSelection(user_select, userChoice);
+    updateSelection(computer_select, computerChoice);
+
+    if(userChoice === computerChoice){
+        //draw
+        winner.innerText = 'draw';
+    }else if(userChoice === 'paper' && computerChoice === 'rock' || userChoice === 'rock' && computerChoice === 'scissors' || userChoice === 'scissors' && computerChoice === 'paper') 
+    {
+        //user won
+        updateScore(1);
+        winner.innerText = 'win';
+    } else {
+        //user lost
+        updateScore(-1);
+        winner.innerText = 'lose';
+    } 
+
+    // show the selection | hide main
+    main.style.display = 'none';
+    selection.style.display = 'flex';
+}
+
+function updateScore(value) {
+    score += value;
+
+    scoreEl.innerText = score;
+    
+}
+
+function pickRandomChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function updateSelection(selectionEl, choice) {
+    
+    // class reset
+    selectionEl.classList.remove('btn-paper');
+    selectionEl.classList.remove('btn-rock');
+    selectionEl.classList.remove('btn-scissors');
+    
+    // update the img
+    const img = selectionEl.querySelector('img');
+    selectionEl.classList.add(`btn-${choice}`);
+    img.src = `./images/icon-${choice}.svg`;
+    img.alt = choice;
+    
+}
+
